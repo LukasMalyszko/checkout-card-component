a846a65f462ed7362794418cc8c4cafe938be76b
Lukasz Malyszko <lukas.malyszko@gmail.com>
2023-05-15 15:30:55 +0200

    checkout card component added

---
+++ src/pages/home-page/home-page.js
@@ -0,0 +1,7 @@
+/// Homepage JavaScript File
+/// Here we import all the JavaScript files we need for our homepage.
+
+import './home-page.scss';
+import { CheckoutCardComponent } from './components/checkout-card-component/checkout-card-component.js';
+
+CheckoutCardComponent();
